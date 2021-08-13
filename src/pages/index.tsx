8a900bf3d784fbd394a765b5500e1edf45c6adab
ardotheedu <eduardosalima3@gmail.com>
2021-08-13 18:13:09 -0300

    feat: display Loafing and Error components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,36 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface ImageData {
+  title: string;
+  description: string;
+  url: string;
+  id: string;
+}
+
+interface ImagesQueryResponse {
+  after?: {
+    id: string;
+  };
+  data: {
+    data: {
+      title: string;
+      description: string;
+      url: string;
+    };
+    ts: number;
+    ref: {
+      id: string;
+    };
+  }[];
+}
+
 export default function Home(): JSX.Element {
+  const fetchProjects = ({ pageParam = 0 }) => api.get<ImagesQueryResponse>('/api/images', {
+    params: {
+      after: pageParam,
+    }
+  });
   const {
     data,
     isLoading,
@@ -18,18 +47,88 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    // (DONE) AXIOS REQUEST WITH PARAM
+    fetchProjects,
+    {
+      // (DONE) GET AND RETURN NEXT PAGE PARAM
+      getNextPageParam: (lastPage, pages) =>  {
+        return lastPage.data.after || null;
+      }
+    }
   );
+
+  const dataa = {
+    "pages": [
+      {
+        "data": [
+          {
+            "data": {
+              "title": "Doge",
+              "description": "The best doge",
+              "url": "https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg",
+            },
+            "ref": {
+              "id": "294961059684418048"
+            },
+              "ts": 1620222828340000,
+          },
+          {
+            "data": {
+              "title": "Doge",
+              "description": "The best doge",
+              "url": "https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg",
+            },
+            "ref": {
+              "id": "294961059684418048"
+            },
+              "ts": 1620222828340000,
+          },
+          {
+            "data": {
+              "title": "Doge",
+              "description": "The best doge",
+              "url": "https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg",
+            },
+            "ref": {
+              "id": "294961059684418048"
+            },
+              "ts": 1620222828340000,
+          },
+        ],
+        "after": "295991160078336512"
+      }
+    ],
+  }
+
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return dataa.pages[0].data.map((image) => {
+      return {
+        title: image.data.title,
+        description: image.data.description,
+        url: image.data.url,
+        id: image.ref.id,
+        ts: image.ts,
+      };
+    }).flat()
+
   }, [data]);
+
+  
+  console.log(formattedData);
 
   // TODO RENDER LOADING SCREEN
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   // TODO RENDER ERROR SCREEN
+
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
