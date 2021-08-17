5bc0b21125bb5958367688aea24db47b2f3ac33c
ardotheedu <eduardosalima3@gmail.com>
2021-08-17 12:10:45 -0300

    feat: List all cards

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,26 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function viewImage(url: string) {
+    setSelectedImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={40}>
+        {cards.map(card => (
+          <Card data={card} viewImage={() => viewImage(card.url)}/>
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage imgUrl={selectedImageUrl} onClose={onClose} isOpen={isOpen} />
     </>
   );
 }
